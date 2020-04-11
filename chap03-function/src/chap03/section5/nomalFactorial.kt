4c5ca9d7b9a293a5478fbac0e51c393f22e3678d
dlgoslaek1 <dlgoslaek1@gmail.com>
2020-04-11 23:28:35 +0900

    commiit

--- chap03-function/src/chap03/section5/nomalFactorial.kt
+++ chap03-function/src/chap03/section5/nomalFactorial.kt
@@ -1,2 +1,14 @@
 package chap03.section5
 
+fun main(){
+    val number = 4
+    val result :Long
+
+    result = factorial(number)
+    println("factorial: $number -> $result")
+
+}
+
+fun factorial(n:Int):Long{
+    return if(n==1) n.toLong() else n* factorial(n-1)
+}
--- chap03-function/src/chap03/section5/tailRecFactorial.kt
+++ chap03-function/src/chap03/section5/tailRecFactorial.kt
@@ -1,2 +1,14 @@
 package chap03.section5
 
+fun main(){
+    val number = 4
+    val result :Long
+
+    result = factorial(number)
+    println("factorial: $number -> $result")
+
+}
+
+tailrec fun factorial(n:Int, run:Int = 1):Long{
+    return if(n==1) n.toLong() else factorial(n-1,run*n)
+}
